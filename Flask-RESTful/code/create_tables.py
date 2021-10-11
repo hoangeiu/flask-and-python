cda8bd38641d6559fa30550067122083ed736240
Lê Minh Hoàng <hoang.le.k3set@eiu.edu.vn>
2021-10-11 11:37:30 +0700

    implement SQLite to manager users and items

---
+++ Flask-RESTful/code/create_tables.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+create_tables = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+cursor.execute(create_tables)
+
+create_tables = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cursor.execute(create_tables)
+
+# cursor.execute("INSERT INTO items VALUES ('piano', 10.99)")
+
+connection.commit()
+
+connection.close()
